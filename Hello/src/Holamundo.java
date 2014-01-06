b478af9bd29ba0dd8a7557ec7c85e21988fb31e4
Antonio carrbonell <acmtibi@gmail.com>
2014-01-06 12:04:36 +0100

    Cambio pantalla mas largo

--- Hello/src/Holamundo.java
+++ Hello/src/Holamundo.java
@@ -3,7 +3,7 @@ public class Holamundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Buenos diass");
+		System.out.println("Buenos diass desde el espacio");
 	}
 
 }
